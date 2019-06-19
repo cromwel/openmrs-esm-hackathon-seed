e3450a60028801917bb6e0196ef4695843a3e95d
cromwel <cromwellindy@gmail.com>
2019-06-19 10:03:14 +0300

    added visits and vitals widget on patient dashboard

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Root(props: RootProps) {
-  return <div className="test">Hackathon seed is working!</div>;
+  return <div className="test">Hackathon Widget!</div>;
 }
 
 type RootProps = {};
